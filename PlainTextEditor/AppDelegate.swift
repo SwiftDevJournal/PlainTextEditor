d290756876a9b3c066aecd5ab05e8b50b1ededa5
Mark Szymczyk <mark@meandmark.com>
2019-08-12 18:11:31 -0400

    Initial Commit

---
+++ PlainTextEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PlainTextEditor
+//
+//  Created by mark on 8/12/19.
+//  Copyright © 2019 Swift Dev Journal. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PlainTextEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PlainTextEditor
+//
+//  Created by mark on 8/12/19.
+//  Copyright © 2019 Swift Dev Journal. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
